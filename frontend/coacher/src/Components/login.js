3d42f94c96d91b9c16eb23e88d946b90a56d9745
Swapnil Jha <59622008+Swapnil0506@users.noreply.github.com>
2021-11-10 13:13:39 +0530

    login done

---
+++ frontend/coacher/src/Components/login.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../style/styles.css";
+import "../style/register.css";
+
+const Login = (props) => {
+    return (
+        <div className ="Login">
+            <form action=" " method="post" >
+            <h2>Sign Up</h2>
+                <br />
+                <hr/>
+                <div className="form-group">
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text">
+                                <i className="fa fa-user"></i>
+                            </span>                    
+                        </div>
+                        <input type="text" className="form-control" name="username" placeholder="First Name" required="required" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text">
+                                <i className="fa fa-user"></i>
+                            </span>                    
+                        </div>
+                        <input type="text" className="form-control" name="lastname" placeholder="Last Name" required="required" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text">
+                                <i className="fa fa-paper-plane"></i>
+                            </span>                    
+                        </div>
+                        <input type="email" className="form-control" name="email" placeholder="Email Address" required="required" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text">
+                                <i className="fa fa-lock"></i>
+                            </span>                    
+                        </div>
+                        <input type="password" className="form-control" name="password" placeholder="Password" required="required" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text">
+                                <i className="fa fa-lock"></i>
+                                <i className="fa fa-check"></i>
+                            </span>                    
+                        </div>
+                        <input type="password" className="form-control" name="confirm_password" placeholder="Confirm Password" required="required" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <select id="select" className="form-control" aria-placeholder="Type of User" required="required">
+                        <option>Student</option>
+                        <option>Faculty</option>
+                    </select>
+                </div>
+                <br />
+                <div className="form-group text-center">
+                    <label className="form-check-label">
+                        <input type="checkbox" required="required" id="myCheckbox" /> I accept the <Link to="/terms">Terms of Use</Link> &amp; <Link to="/privacy">Privacy Policy</Link>
+                    </label>
+                </div>
+                <div className="form-group text-center">
+                    <button type="submit" className="btn btn-primary btn-lg" id="myButton" disabled>Register</button>
+                    <script type="text/javascript">
+                    $('#myCheckbox').click(function() {
+                        $('#myButton').attr("disabled", !this.checked)
+                    });
+                    </script>
+                </div><br />
+                <div className="text-center" style={{ marginBottom: "10px" }}>Already have an account? <Link to="/login" style="text-decoration:none;">Login here</Link></div>
+            </form >
+        </div >
+    );
+}
+
+export default Login;
