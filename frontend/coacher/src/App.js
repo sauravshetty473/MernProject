00fa50e7329142c70594329deac19b711567bec5
vidhi.0311shah <vidhi11@somaiya.edu>
2021-11-13 23:31:19 +0530

    Changes in overview of test page

--- frontend/coacher/src/App.js
+++ frontend/coacher/src/App.js
@@ -8,7 +8,6 @@ function App() {
 		<div>
 			<Router>
 				<Header />
-				<Test />
 			</Router>
 		</div>
 	);
---
+++ frontend/coacher/src/Components/testcards.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class TestCard extends Component {
+	render() {
+		return <div></div>;
+	}
+}
+export default TestCard;
