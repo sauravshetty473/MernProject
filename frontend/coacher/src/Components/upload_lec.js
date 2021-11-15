209199b755451e1f2bbf11b346c7c44ef0995456
Swapnil Jha <59622008+Swapnil0506@users.noreply.github.com>
2021-11-15 22:26:46 +0530

    Lectures done

---
+++ frontend/coacher/src/Components/upload_lec.js
@@ -0,0 +1,79 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import Navbar from "./navbar";
+import "../style/style.css";
+import "../style/upload.css";
+
+class Upload extends React.Component {
+    render() {
+        return (
+            <div>
+            <p>Wondering where to start? Here are some tips and templates from Biteable to get you going on your first how-to video.</p>
+            <h2 id="loc-what-makes-a-good-how-to-video">What makes a good how-to video?</h2>
+            <p>Once you have your topic, you need to hit the perfect balance between engaging and informative. The best videos strike a middle ground between visually interesting content and a topic that can be broken down into bite-sized pieces for your audience. Keep your video to-the-point, use an engaging title, 
+            and consider adding captions or animations for audiences watching without sound.</p>
+            <div class="ContentBlocks__component">                                                 
+                <div class="MediaSoloBlock MediaSoloBlock--wide">           
+                    <div class="Media Media--rounded" id="media-youtube-Q-a2FR1iwqg">                                                       
+                        <figure class="Media__fig">           
+                            <iframe 
+                                Link="https://www.youtube.com/embed/Q-a2FR1iwqg" srcDoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:1.5rem/1.7 sans-serif;color:white;background:rgba(0,0,0,.7);padding:.4rem 1.4rem;width:auto;left:50%;transform:translateX(-50%);clip-path:ellipse(4rem 1.5rem);border-radius:.95rem}span:hover{background-color:red}</style><a href='https://www.youtube.com/embed/Q-a2FR1iwqg?autoplay=1&amp;rel=0&amp;showinfo=0&amp;controls=1'><img loading='lazy' src='https://i.ytimg.com/vi/Q-a2FR1iwqg/maxresdefault.jpg' alt=''><span>▶</span></a>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true">
+                            </iframe>         
+                        </figure>                   
+                    </div> 
+                </div>                           
+            </div>
+            <br/>
+            <p>Wondering where to start? Here are some tips and templates from Biteable to get you going on your first how-to video.</p>
+            <h2 id="loc-what-makes-a-good-how-to-video">What makes a good how-to video?</h2>
+            <p>Once you have your topic, you need to hit the perfect balance between engaging and informative. The best videos strike a middle ground between visually interesting content and a topic that can be broken down into bite-sized pieces for your audience. Keep your video to-the-point, use an engaging title, 
+            and consider adding captions or animations for audiences watching without sound.</p>
+            <div class="ContentBlocks__component">                                                 
+                <div class="MediaSoloBlock MediaSoloBlock--wide">           
+                    <div class="Media Media--rounded" id="media-youtube-Q-a2FR1iwqg">                                                       
+                        <figure class="Media__fig">           
+                            <iframe 
+                                Link="https://www.youtube.com/embed/Q-a2FR1iwqg" srcDoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:1.5rem/1.7 sans-serif;color:white;background:rgba(0,0,0,.7);padding:.4rem 1.4rem;width:auto;left:50%;transform:translateX(-50%);clip-path:ellipse(4rem 1.5rem);border-radius:.95rem}span:hover{background-color:red}</style><a href='https://www.youtube.com/embed/Q-a2FR1iwqg?autoplay=1&amp;rel=0&amp;showinfo=0&amp;controls=1'><img loading='lazy' src='https://i.ytimg.com/vi/Q-a2FR1iwqg/maxresdefault.jpg' alt=''><span>▶</span></a>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true">
+                            </iframe>         
+                        </figure>                   
+                    </div> 
+                </div>                           
+            </div>
+            <br/>
+            <p>Wondering where to start? Here are some tips and templates from Biteable to get you going on your first how-to video.</p>
+            <h2 id="loc-what-makes-a-good-how-to-video">What makes a good how-to video?</h2>
+            <p>Once you have your topic, you need to hit the perfect balance between engaging and informative. The best videos strike a middle ground between visually interesting content and a topic that can be broken down into bite-sized pieces for your audience. Keep your video to-the-point, use an engaging title, 
+            and consider adding captions or animations for audiences watching without sound.</p>
+            <div class="ContentBlocks__component">                                                 
+                <div class="MediaSoloBlock MediaSoloBlock--wide">           
+                    <div class="Media Media--rounded" id="media-youtube-Q-a2FR1iwqg">                                                       
+                        <figure class="Media__fig">           
+                            <iframe 
+                                Link="https://www.youtube.com/embed/Q-a2FR1iwqg" srcDoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:1.5rem/1.7 sans-serif;color:white;background:rgba(0,0,0,.7);padding:.4rem 1.4rem;width:auto;left:50%;transform:translateX(-50%);clip-path:ellipse(4rem 1.5rem);border-radius:.95rem}span:hover{background-color:red}</style><a href='https://www.youtube.com/embed/Q-a2FR1iwqg?autoplay=1&amp;rel=0&amp;showinfo=0&amp;controls=1'><img loading='lazy' src='https://i.ytimg.com/vi/Q-a2FR1iwqg/maxresdefault.jpg' alt=''><span>▶</span></a>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true">
+                            </iframe>         
+                        </figure>                   
+                    </div> 
+                </div>                           
+            </div>
+            <br/>
+            <p>Wondering where to start? Here are some tips and templates from Biteable to get you going on your first how-to video.</p>
+            <h2 id="loc-what-makes-a-good-how-to-video">What makes a good how-to video?</h2>
+            <p>Once you have your topic, you need to hit the perfect balance between engaging and informative. The best videos strike a middle ground between visually interesting content and a topic that can be broken down into bite-sized pieces for your audience. Keep your video to-the-point, use an engaging title, 
+            and consider adding captions or animations for audiences watching without sound.</p>
+            <div class="ContentBlocks__component">                                                 
+                <div class="MediaSoloBlock MediaSoloBlock--wide">           
+                    <div class="Media Media--rounded" id="media-youtube-Q-a2FR1iwqg">                                                       
+                        <figure class="Media__fig">           
+                            <iframe 
+                                Link="https://www.youtube.com/embed/Q-a2FR1iwqg" srcDoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:1.5rem/1.7 sans-serif;color:white;background:rgba(0,0,0,.7);padding:.4rem 1.4rem;width:auto;left:50%;transform:translateX(-50%);clip-path:ellipse(4rem 1.5rem);border-radius:.95rem}span:hover{background-color:red}</style><a href='https://www.youtube.com/embed/Q-a2FR1iwqg?autoplay=1&amp;rel=0&amp;showinfo=0&amp;controls=1'><img loading='lazy' src='https://i.ytimg.com/vi/Q-a2FR1iwqg/maxresdefault.jpg' alt=''><span>▶</span></a>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true">
+                            </iframe>         
+                        </figure>                   
+                    </div> 
+                </div>                           
+            </div>
+            </div>
+        );
+    }
+}
+
+export default Upload;
