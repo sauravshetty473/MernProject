0b26204515c492c9aa4b0ad00779dd50d3281d35
sauravshetty473 <saurav.vs@somaiya.edu>
2021-11-21 10:50:54 +0530

    ye boiiii

--- server.js
+++ server.js
@@ -12,11 +12,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+const usersRouter = require('./routes/users');
+
+
 const uri = process.env.ATLAS_URI;
 mongoose
     .connect( uri, { useNewUrlParser: true,useUnifiedTopology: true })
-    .then(() =>app.listen(port, ()=>{
+    .then(() => {
+
+
+        app.use('/users', usersRouter);
+
+
+
+
+
+        app.listen(port, ()=>{
         console.log('Server is running on port: ${port}');
-    }))
+
+
+
+
+
+    })})
     .catch(err => console.log( err ));
 
